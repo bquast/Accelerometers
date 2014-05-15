f56864f1554c5b40ad4facf7d728703c57d03b2e
Bastiaan Quast <bquast@gmail.com>
2014-05-15 14:24:04 +0200

    functioning import and labelling

--- run_analysis.R
+++ run_analysis.R
@@ -16,3 +16,31 @@ file.dest <- 'accelerometer.zip'
 download.file(file.url, file.dest)
 # NOTE: because of the size, this file is ignored by Git
 
+# cleanup
+rm(file.url)
+rm(file.dest)
+
+# UNZIP the file manually
+
+# read the data
+features <- read.table('UCI HAR Dataset/features.txt')
+features <- t(features[2])
+y_train <- read.table('UCI HAR Dataset/train/y_train.txt')
+y_test <- read.table('UCI HAR Dataset/test/y_test.txt')
+labs <- c('walking', 'walking_upstairs', 'walking_downstairs', 'sitting', 'standing', 'laying')
+y_train <- as.data.frame(factor(y_train$V1, levels=c(1,2,3,4,5,6), labels=labs) )
+y_test <- as.data.frame(factor(y_test$V1, levels=c(1,2,3,4,5,6), labels=labs) )
+train <- read.table('UCI HAR Dataset/train/X_train.txt')
+test <- read.table('UCI HAR Dataset/test/X_test.txt')
+# add activity variable
+train <- as.data.frame( c(y_train, train) )
+test <- as.data.frame( c(y_test, test) )
+
+# remove temporary variables
+rm(features)
+rm(labs)
+rm(y_train)
+rm(y_test)
+
+# save the workspace
+save.image('import.RData')
